2c92689476ab99f843726445caec47e95f8b43ed
Fer Sánchez <sangarfer@gmail.com>
2022-02-10 18:21:21 +0100

    lab finished

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv/config");
+
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -5,7 +7,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PASSWORD}@cluster0.5zqfb.mongodb.net/recipe-app?authSource=admin&replicaSet=atlas-ffdmfo-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
 
 // Connection to the database "recipe-app"
 mongoose
@@ -13,11 +15,43 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.deleteMany()
+    .then(() => {
+      Recipe.create({
+        title: "Pollo empanao con papas",
+        level: "Easy Peasy",
+        ingredients: [
+          "1 pechuga de pollo",
+          "pan rallao",
+          "2 huevos",
+          "aceite de oliva extra",
+          "2 limones",
+          "3 papas",
+          "sal al gusto"
+        ],
+        cuisine: "Española",
+        dishType: "main_course",
+        image: "https://eldoradomalaga.com/wp-content/uploads/2020/05/pollo_empanado_con_patatas.jpg",
+        duration: 30,
+        creator: "Receta de la abuela"
+      })
+      .then(()=>{
+        Recipe.insertMany(data)
+        .then(()=>{
+          Recipe.findOneAndUpdate(
+            {title: "Rigatoni alla Genovese"},
+            {duration: 100},
+            {new: true})
+          .then(async ()=>{
+            await Recipe.deleteOne({title: "Carrot Cake"})
+            await mongoose.connection.close()
+          })
+        })
+      })
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+//mongoose.connection.close();
